7e52ae79a4a645aa50c84bc3cac123af38abd57d
jacekWachowicz <j.wut@wp.pl>
2026-07-13 00:39:40 +0200

    dodanie pozostałych typów -do przetestowania

--- main.c
+++ main.c
@@ -160,8 +160,12 @@ int main(){
     EDO.kara=2.0; EDO.czas=10*12; EDO.prc = 5.35; EDO.okresWyplaty=0; EDO.nazwa="EDO\0";
     typObligacji COI;
     COI.kara=2.0; COI.czas=4*12; COI.prc = 4.75; COI.okresWyplaty=12; COI.nazwa="COI\0";
-    typObligacji COI2;
-    COI2.kara=2.0; COI2.czas=4*12; COI2.prc = 4.75; COI2.okresWyplaty=0; COI2.nazwa="COI2\0";
+    typObligacji ROR;
+    ROR.kara=0.5; ROR.czas=12; ROR.prc = 4.0; ROR.okresWyplaty=1; ROR.nazwa="ROR\0";
+    typObligacji DOR;
+    DOR.kara=0.7; DOR.czas=2*12; DOR.prc = 4.15; DOR.okresWyplaty=1; DOR.nazwa="DOR\0";
+    typObligacji OTS;
+    OTS.kara=99.0; OTS.czas=3 ; OTS.prc = 2.0; OTS.okresWyplaty=0; OTS.nazwa="OTS\0";
     Parameters parameters;
 
     printf("Wyświetlać szczegóły?(y/n): ");
@@ -169,21 +173,32 @@ int main(){
     if(choice=='y')parameters.verbose=1;
     else parameters.verbose=0; 
 
-    printf("Jaki typ obligacji? 1-EDO, 2-TOS, 3-COI\n");
+    printf("Jaki typ obligacji?\n");
+    printf("1. 3 miesięczne OTS (%.2f%%)\n2. 1 roczne ROR (%.2f%%)\n3. 2 letnie DOR (%.2f%%)\n4. 3 letnie TOS (%.2f%%)\n5. 4 letnie COI (%.2f%%)\n6. 10 letnie EDO (%.2f%%)\n",
+            OTS.prc,ROR.prc,DOR.prc,TOS.prc,COI.prc,EDO.prc);
     scanf("%d",&typ);
     switch (typ){
         case 1:
-            parameters.typ=EDO;
+            parameters.typ=OTS;
             break;
         case 2:
-            parameters.typ=TOS;
+            parameters.typ=ROR;
             break;
         case 3:
+            parameters.typ=DOR;
+            break;
+        case 4:
+            parameters.typ=TOS;
+            break;
+        case 5:
             parameters.typ=COI;
             break;
-        default:
-            parameters.typ=COI2;
+        case 6:
+            parameters.typ=EDO;
             break;
+        default:
+            printf("Niepoprawny typ obligacji\n");
+            return 1;
     }
 
     printf("Po jakim czasie chcesz wyjąć pieniądze?\n(np. 12m - 12 miesięcy, 10l - 10 lat)\n");
